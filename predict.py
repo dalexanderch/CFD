34f7561fd7150b23cab5d071e1a1d1505de20d90
David Alexander <david.alexander@etu.unige.ch>
2019-09-19 17:08:29 +0200

    Updated predict

--- predict.py
+++ predict.py
@@ -49,36 +49,3 @@ for i, image in enumerate(small):
 	predicted_img = Image.fromarray(predicted_img, mode='L')
 	predicted_img.save(cur + pathBig + "{}.gif".format(i), format = 'GIF')
 	print("Saved image {}".format(i))
-	# save 
-# # Build path
-# cur_dir = os.getcwd()
-# pathoriginal = "/data/mnist/"
-# pathoriginal = cur_dir + pathoriginal
-# print(pathoriginal)
-# # Save first thousand
-# for id, image in enumerate(x_train[0:1000,:,:]):
-# 	image = image.astype("uint8")
-# 	image = Image.fromarray(image, mode='L')
-# 	image.save(pathoriginal + "{}.gif".format(id), format = 'GIF')
-
-# # Load model 
-# autoencoder = load_model('autoencoder.h5')
-# autoencoder.summary()
-	
-# # Predict and save
-# images = sorted(os.listdir(pathoriginal)) 
-# pathsave = "/data/pred/"
-# pathsave = cur_dir + pathsave
-# for id, filename in enumerate(images):
-# 	image = Image.open(pathoriginal + filename)
-# 	image = np.asarray(image)
-# 	image = image.flatten()
-# 	image = image.astype('float32') / 255
-# 	image = np.array([image])
-# 	predicted_img = autoencoder.predict(image, verbose=0)
-# 	# save predicted_image
-# 	predicted_img = predicted_img.reshape(56,56)
-# 	predicted_img = 255 * predicted_img
-# 	predicted_img = predicted_img.astype('int8')
-# 	predicted_img = Image.fromarray(predicted_img, mode='L')
-# 	predicted_img.save(pathsave + "{}.gif".format(id), format = 'GIF')
