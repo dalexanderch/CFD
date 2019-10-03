b3599d242b9da1758d7c45a2991844d27f7eaf1c
David Alexander <david.alexander@etu.unige.ch>
2019-10-03 14:49:18 +0200

    Updated prepdata

--- celeba/prepdata.py
+++ celeba/prepdata.py
@@ -1,17 +1,20 @@
 from PIL import Image
 import os, sys
+import time
 
-# Run only once to downscale the images once and for all
+# Run only once to downscale the images and convert to grayscale once and for all
 curr = os.getcwd()
 path = "/data/big/data/"
 pathsave = "/data/small/data/"
-
 dirs = os.listdir( curr + path )
-for index, item in enumerate(dirs):
-	print (item)
-    if os.path.isfile(path+item):
-        im = Image.open(path+item)
-        f, e = os.path.splitext(path+item)
-        imResize = im.resize((89,109), Image.BILINEAR)
-        imResize.save(pathsave + f, 'JPEG')
+start = time.time()
 
+for index, item in enumerate(dirs):
+	print(index)
+	im = Image.open(curr + path + item)
+	im = im.convert('L')
+	imResize = im.resize((89,109), Image.BILINEAR)
+	imResize.save(curr + pathsave + item, 'JPEG')
+
+end = time.time()
+print("Time elapsed : {}".format(end - start))
