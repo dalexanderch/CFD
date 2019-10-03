66e121565aeb699b8bfeb98cb3c653836e415a5c
David Alexander <david.alexander@etu.unige.ch>
2019-10-03 13:59:14 +0200

    Updated celeba

---
+++ celeba/prepdata.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os, sys
+
+# Run only once to downscale the images once and for all
+curr = os.getcwd()
+path = "/data/big/data/"
+pathsave = "/data/small/data/"
+
+dirs = os.listdir( curr + path )
+
+
+    for index, item in enumerate(dirs):
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((89,109), Image.BILINEAR)
+            imResize.save(pathsave + f, 'JPEG')
+
+resize()
