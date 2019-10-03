8a22aae41b80d78c912fd88c2134a493f70e1e38
David Alexander <david.alexander@etu.unige.ch>
2019-10-03 23:38:07 +0200

    Updated cfd

--- cfd/upsample.py
+++ cfd/upsample.py
@@ -1,80 +1,53 @@
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
-from keras.models import Model
-from keras.datasets import mnist
-import tensorflow as tf
-import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
-import os
-from sklearn.model_selection import train_test_split
-import sys
-from keras import backend as K
+from keras.layers import Input, Dense, Conv2D, UpSampling2D
+from keras.models import Model
+import numpy as np
 
+def gen(it1, it2):
+    while True:
+        X = it1.next()
+        Y = it2.next()
+        yield X,Y
 
-#################### Parameters
-epochs = int(sys.argv[1])
-batch_size = int(sys.argv[2])
-if tf.test.gpu_device_name():
-	print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
-else:
-	print("Please install GPU version of TF")
+# Parameters 
+batch_size = 32
+epochs = 20
 
+# Create generator
+datagen = ImageDataGenerator(validation_split=0.1, rescale=1./255)
 
-#################### Load data
-def load_data( path ): # path with respect to current directy
-	cur_dir = os.getcwd()
-	path = cur_dir + path
-	images = sorted(os.listdir(path)) 
+# Prepare training and validation  datasets
+train_it = datagen.flow_from_directory(directory='datacfd/big/', target_size=(200,80), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training')
+val_it = datagen.flow_from_directory(directory='datacfd/big/', target_size=(200,80), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation')
+train_small_it  = datagen.flow_from_directory(directory='datacfd/small/', target_size=(100,40), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training', interpolation = 'bilinear')
+val_small_it =  datagen.flow_from_directory(directory='datacfd/small/', target_size=(100,40), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation', interpolation = 'bilinear')
 
-	data = []
-	for filename in images:
-		image = Image.open(path + filename)
-		image = np.asarray(image)
-		image = image.astype('float32') / 255
-		data.append(np.asarray(image)) 
-
-	data = np.array(data)
-	return data
-
-# load small images
-small = load_data("/data/small/")
-
-# load big images 
-big = load_data("/data/big/")
-
-# split into training set and validation set 
-x_train, x_test, y_train, y_test = train_test_split(small, big, test_size=0.2) # We take 20% of samples for validation
-
-# adapt for use with tf
-x_train = np.expand_dims(x_train[:, 0:x_train.shape[1] - 1, 0:x_train.shape[2] - 1], axis=3)
-x_test = np.expand_dims(x_test[:, 0:x_test.shape[1] - 1, 0:x_test.shape[2] - 1], axis=3)
-y_train = np.expand_dims(y_train[:, 0:y_train.shape[1] - 1, 0:y_train.shape[2] - 1], axis=3)
-y_test = np.expand_dims(y_test[:, 0:y_test.shape[1] - 1, 0:y_test.shape[2] - 1], axis=3)
-
-#################### Build model
-input_img = Input(shape=(x_train.shape[1], x_train.shape[2], 1))  # adapt this if using `channels_first` image data format
-x = UpSampling2D((2, 2))(input_img)
+# Build model
+input_img = Input(shape=(100, 40, 1))  # adapt this if using `channels_first` image data format
+x = UpSampling2D((2, 2), interpolation='bilinear')(input_img)
 x = Conv2D(64, (9, 9), activation='relu', padding='same')(x)
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 x = Conv2D(1, (3, 3), activation='relu', padding='same')(x)
 
-
 upsample = Model(input_img, x)
 upsample.compile(optimizer='adadelta', loss='mean_squared_error')
-upsample.summary()
 
-#################### Train the model
-upsample.fit(x_train, y_train,
-                epochs=epochs,
-                batch_size=batch_size,
-                shuffle=True,
-                validation_data=(x_test, y_test))
+# Train
+g_train = gen(train_small_it, train_it)
+g_val = gen(val_small_it, val_it)
 
+upsample.fit_generator(
+	generator = g_train,
+	steps_per_epoch = 5727, # 183240/32 rounded upward
+	epochs = 1,
+	validation_data = g_val,
+	validation_steps = 634, # 20259/256 rounded upward
+	use_multiprocessing=True
+	)
 
-
-#################### Save model
+# Save weights
 upsample.save("upsample.h5")
 
-
-
-
-
+# Evaluate
+print(upsample.evaluate_generator(generator = g_val, steps=634, use_multiprocessing=True))
