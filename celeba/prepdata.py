ce09ad9b2532f59c5a6f9a9fd6c6ab5ad67ff3d8
David Alexander <david.alexander@etu.unige.ch>
2019-10-03 14:04:37 +0200

    Updated prepdata

--- celeba/prepdata.py
+++ celeba/prepdata.py
@@ -7,13 +7,10 @@ path = "/data/big/data/"
 pathsave = "/data/small/data/"
 
 dirs = os.listdir( curr + path )
+for index, item in enumerate(dirs):
+    if os.path.isfile(path+item):
+        im = Image.open(path+item)
+        f, e = os.path.splitext(path+item)
+        imResize = im.resize((89,109), Image.BILINEAR)
+        imResize.save(pathsave + f, 'JPEG')
 
-
-    for index, item in enumerate(dirs):
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
-            f, e = os.path.splitext(path+item)
-            imResize = im.resize((89,109), Image.BILINEAR)
-            imResize.save(pathsave + f, 'JPEG')
-
-resize()
