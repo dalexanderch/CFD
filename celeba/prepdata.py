b2ae4c1f76eb1f8f3b4661a5c9fb4923eef8a349
David Alexander <david.alexander@etu.unige.ch>
2020-02-04 21:23:32 +0100

    Updated pipeline

--- celeba/prepdata.py
+++ celeba/prepdata.py
@@ -4,8 +4,8 @@ import time
 
 # Run only once to downscale the images and convert to grayscale once and for all
 curr = os.getcwd()
-path = "/data/img/"
-pathsave = "/data/imgsmall/"
+path = "/data/databig/img/"
+pathsave = "/data/datasmall/imgsmall/"
 dirs = os.listdir( curr + path )
 start = time.time()
 
--- celeba/upclassic.py
+++ celeba/upclassic.py
@@ -11,17 +11,17 @@ def gen(it1, it2):
         yield X,Y
 
 # Parameters
-batch_size = 32
-epochs = 20
+epochs = int(sys.argv[1])
+batch_size = int(sys.argv[2])
 
 # Create generator
 datagen = ImageDataGenerator(validation_split=0.1, rescale=1./255)
 
 # Prepare training and validation  datasets
-train_it = datagen.flow_from_directory(directory='data/img/', target_size=(178,218), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training')
-val_it = datagen.flow_from_directory(directory='data/img/', target_size=(178,218), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation')
-train_small_it  = datagen.flow_from_directory(directory='data/imgsmall/', target_size=(89,109), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training', interpolation = 'bilinear')
-val_small_it =  datagen.flow_from_directory(directory='data/imgsmall/', target_size=(89,109), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation', interpolation = 'bilinear')
+train_it = datagen.flow_from_directory(directory='data/databig', target_size=(178,218), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training')
+val_it = datagen.flow_from_directory(directory='data/databig/', target_size=(178,218), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation')
+train_small_it  = datagen.flow_from_directory(directory='data/datasmall', target_size=(89,109), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training', interpolation = 'bilinear')
+val_small_it =  datagen.flow_from_directory(directory='data/datasmall', target_size=(89,109), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation', interpolation = 'bilinear')
 
 # Build model
 input_img = Input(shape=(89, 109, 1))  # adapt this if using `channels_first` image data format
