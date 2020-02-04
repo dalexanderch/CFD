2efbb354043a56970a5b14bd5f1d28d0a5a1bd36
David Alexander <david.alexander@etu.unige.ch>
2020-02-04 20:27:14 +0100

    fixed pipeline

--- celeba/prepdata.py
+++ celeba/prepdata.py
@@ -10,6 +10,7 @@ dirs = os.listdir( curr + path )
 start = time.time()
 
 for index, item in enumerate(dirs):
+	print(index)
 	im = Image.open(curr + path + item)
 	im = im.convert('L')
 	imResize = im.resize((89,109), Image.BILINEAR)
