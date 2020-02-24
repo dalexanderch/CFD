771655c31967be767b09ce3ad94025758a2a4e2c
David Alexander <david.alexander@etu.unige.ch>
2020-02-24 18:20:24 +0100

    celeba

--- celeba/upsample.py
+++ celeba/upsample.py
@@ -51,10 +51,10 @@ g_val = gen(val_small_it, val_it)
 
 upsample.fit_generator(
 	generator = g_train,
-	steps_per_epoch = 5727, # 183240/32 rounded upward
+	steps_per_epoch = 1432, # 183240/32 rounded upward
 	epochs = 20,
 	validation_data = g_val,
-	validation_steps = 634, # 20259/256 rounded upward
+	validation_steps = 159, # 20259/256 rounded upward
 	use_multiprocessing=True
 	)
 
