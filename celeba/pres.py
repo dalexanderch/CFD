283a11862f14fc580a5e1e8c80098455dcc8d45c
David Alexander <david.alexander@etu.unige.ch>
2020-02-24 22:54:37 +0100

    pres

--- celeba/pres.py
+++ celeba/pres.py
@@ -6,16 +6,16 @@ import os
 
 # Save original
 curr = os.getcwd()
-image1 = Image.open(curr + "data/databig/img/000001.jpg")
+image1 = Image.open(curr + "/data/databig/img/000001.jpg")
 image1 = image.convert('L')
 image1.save('img1.jpg', 'JPEG')
-image2 = Image.open(curr + "data/databig/img/000002.jpg")
+image2 = Image.open(curr + "/data/databig/img/000002.jpg")
 image2 = image.convert('L')
 image2.save('img2.jpg', 'JPEG')
-image3 = Image.open(curr + "data/databig/img/000003.jpg")
+image3 = Image.open(curr + "/data/databig/img/000003.jpg")
 image3 = image.convert('L')
 image3.save('img3.jpg', 'JPEG')
-image4 = Image.open(curr + "data/databig/img/000004.jpg")
+image4 = Image.open(curr + "/data/databig/img/000004.jpg")
 image4 = image.convert('L')
 image4.save('img4.jpg', 'JPEG')
 
