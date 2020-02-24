792ef8a574880c5491d9efe218d03b587b47abfd
David Alexander <david.alexander@etu.unige.ch>
2020-02-24 15:29:02 +0100

    pres

--- cifar10/pres.py
+++ cifar10/pres.py
@@ -22,13 +22,13 @@ img1 = img1.astype('uint8')
 img1 = Image.fromarray(img1, mode = 'RGB')
 img1 = img1.convert('L')
 img1.save('img1.png', 'PNG')
-img2 = x_test[0]
+img2 = x_test[1]
 img2 = img2 * 255
 img2 = img2.astype('uint8')
 img2 = Image.fromarray(img2, mode = 'RGB')
 img2 = img2.convert('L')
 img2.save('img2.png', 'PNG')
-img3 = x_test[0]
+img3 = x_test[2]
 img3 = img3 * 255
 img3 = img3.astype('uint8')
 img3 = Image.fromarray(img3, mode = 'RGB')
