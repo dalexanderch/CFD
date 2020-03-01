e291eebd8974c7a5d3dc898fe94d9fcdf25eafb4
David Alexander <david.alexander@etu.unige.ch>
2020-03-01 23:18:56 +0100

    dual

--- cfd/upclassic_dual.py
+++ cfd/upclassic_dual.py
@@ -64,10 +64,10 @@ g_val = gen(val_small_it, val_it)
 
 upsample.fit_generator(
 	generator = g_train,
-	steps_per_epoch = 5727, # 183240/32 rounded upward
+	steps_per_epoch = 45000, 
 	epochs = epochs,
 	validation_data = g_val,
-	validation_steps = 634, # 20259/256 rounded upward
+	validation_steps = 4500,
 	use_multiprocessing=True
 	)
 
