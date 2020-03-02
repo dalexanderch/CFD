9b64961eec91535d04e795f7bfa4d1a2b2142259
David Alexander <david.alexander@etu.unige.ch>
2020-03-02 22:53:38 +0100

    dual

--- cfd/upclassic.py
+++ cfd/upclassic.py
@@ -31,8 +31,8 @@ datagen = ImageDataGenerator(validation_split=0.1, rescale=1./255)
 # Prepare training and validation  datasets
 train_it = datagen.flow_from_directory(directory='data/big/', target_size=(200,80), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training')
 val_it = datagen.flow_from_directory(directory='data/big/', target_size=(200,80), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation')
-train_small_it  = datagen.flow_from_directory(directory='data/small/', target_size=(100,40), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training', interpolation = 'bilinear')
-val_small_it =  datagen.flow_from_directory(directory='data/small/', target_size=(100,40), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation', interpolation = 'bilinear')
+train_small_it  = datagen.flow_from_directory(directory='data/small/', target_size=(100,40), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='training')
+val_small_it =  datagen.flow_from_directory(directory='data/small/', target_size=(100,40), shuffle=False, color_mode='grayscale', class_mode=None, batch_size=batch_size, subset='validation')
 
 # Build model
 input_img = Input(shape=(100, 40, 1))  # adapt this if using `channels_first` image data format
@@ -50,7 +50,7 @@ upsample.fit_generator(
 	steps_per_epoch = math.ceil(45000/batch_size),
 	epochs = epochs,
 	validation_data = g_val,
-	validation_steps = math.ceil(5000/batch_size), 
+	validation_steps = math.ceil(5000/batch_size),
 	use_multiprocessing=True
 	)
 
