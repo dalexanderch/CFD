0daf507bd7eb0d5cfae0454fc695e127e0bc112a
David Alexander <david.alexander@etu.unige.ch>
2020-05-04 13:26:04 +0200

    psnr

--- palabos-v2.0r0/examples/showCases/cylinder2d/animation.py
+++ palabos-v2.0r0/examples/showCases/cylinder2d/animation.py
@@ -131,3 +131,30 @@ ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
                                 repeat_delay=1000)
 
 ani.save('model_psnr.mp4')
+
+# Load model
+upsample = load_model("model_ssim.h5", custom_objects=dependencies )
+# Prepare data
+path = os.getcwd() + "/small"
+files = [f for f in glob.glob(path + "**/*.npy")]
+files = sorted_nicely(files)
+files = files[0:499]
+
+data = []
+for file in files:
+    tmp = np.load(file)
+    tmp = np.reshape(tmp, (1,41,101,1))
+    tmp = upsample.predict(tmp)
+    tmp = np.reshape(tmp, (82,202))
+    data.append(tmp)
+
+fig = plt.figure()
+ims = []
+for d in data:
+    im = plt.imshow(d, animated=True)
+    ims.append([im])
+#Show
+ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                repeat_delay=1000)
+
+ani.save('model_ssim.mp4')
