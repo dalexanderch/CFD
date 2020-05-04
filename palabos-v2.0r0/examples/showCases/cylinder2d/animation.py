ca74242b181b4cd58f639eb5971d54ec385fa8e9
David Alexander <david.alexander@etu.unige.ch>
2020-05-04 12:27:56 +0200

    psnr

--- palabos-v2.0r0/examples/showCases/cylinder2d/animation.py
+++ palabos-v2.0r0/examples/showCases/cylinder2d/animation.py
@@ -29,30 +29,84 @@ dependencies = {
      'PSNR': PSNR
 }
 
-# Prepare data
-path = os.getcwd() + "/big"
-files = [f for f in glob.glob(path + "**/*.npy")]
-files = sorted_nicely(files)
-files = files[0:499]
-
-data = []
-for file in files:
-    tmp = np.load(file)
-    tmp = np.reshape(tmp, (82,202))
-    data.append(tmp)
-
-fig = plt.figure()
-ims = []
-for d in data:
-    im = plt.imshow(d, animated=True)
-    ims.append([im])
-#Show
-ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
-                                repeat_delay=1000)
-ani.save('original.mp4')
+## Prepare data
+#path = os.getcwd() + "/big"
+#files = [f for f in glob.glob(path + "**/*.npy")]
+#files = sorted_nicely(files)
+#files = files[0:499]
+#
+#data = []
+#for file in files:
+#    tmp = np.load(file)
+#    tmp = np.reshape(tmp, (82,202))
+#    data.append(tmp)
+#
+#fig = plt.figure()
+#ims = []
+#for d in data:
+#    im = plt.imshow(d, animated=True)
+#    ims.append([im])
+##Show
+#ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+#                                repeat_delay=1000)
+#ani.save('original.mp4')
+#
+## Load model
+#upsample = load_model("classic.h5", custom_objects=dependencies)
+## Prepare data
+#path = os.getcwd() + "/small"
+#files = [f for f in glob.glob(path + "**/*.npy")]
+#files = sorted_nicely(files)
+#files = files[0:499]
+#
+#data = []
+#for file in files:
+#    tmp = np.load(file)
+#    tmp = np.reshape(tmp, (1,41,101,1))
+#    tmp = upsample.predict(tmp)
+#    tmp = np.reshape(tmp, (82,202))
+#    data.append(tmp)
+#
+#fig = plt.figure()
+#ims = []
+#for d in data:
+#    im = plt.imshow(d, animated=True)
+#    ims.append([im])
+##Show
+#ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+#                                repeat_delay=1000)
+#
+#ani.save('classic.mp4')
+#
+## Load model
+#upsample = load_model("model.h5", custom_objects=dependencies )
+## Prepare data
+#path = os.getcwd() + "/small"
+#files = [f for f in glob.glob(path + "**/*.npy")]
+#files = sorted_nicely(files)
+#files = files[0:499]
+#
+#data = []
+#for file in files:
+#    tmp = np.load(file)
+#    tmp = np.reshape(tmp, (1,41,101,1))
+#    tmp = upsample.predict(tmp)
+#    tmp = np.reshape(tmp, (82,202))
+#    data.append(tmp)
+#
+#fig = plt.figure()
+#ims = []
+#for d in data:
+#    im = plt.imshow(d, animated=True)
+#    ims.append([im])
+##Show
+#ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+#                                repeat_delay=1000)
+#
+#ani.save('model.mp4')
 
 # Load model
-upsample = load_model("classic.h5", custom_objects=dependencies)
+upsample = load_model("model_psnr.h5", custom_objects=dependencies )
 # Prepare data
 path = os.getcwd() + "/small"
 files = [f for f in glob.glob(path + "**/*.npy")]
@@ -76,31 +130,4 @@ for d in data:
 ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
                                 repeat_delay=1000)
 
-ani.save('classic.mp4')
-
-# Load model
-upsample = load_model("model.h5", custom_objects=dependencies )
-# Prepare data
-path = os.getcwd() + "/small"
-files = [f for f in glob.glob(path + "**/*.npy")]
-files = sorted_nicely(files)
-files = files[0:499]
-
-data = []
-for file in files:
-    tmp = np.load(file)
-    tmp = np.reshape(tmp, (1,41,101,1))
-    tmp = upsample.predict(tmp)
-    tmp = np.reshape(tmp, (82,202))
-    data.append(tmp)
-
-fig = plt.figure()
-ims = []
-for d in data:
-    im = plt.imshow(d, animated=True)
-    ims.append([im])
-#Show
-ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
-                                repeat_delay=1000)
-
-ani.save('model.mp4')
+ani.save('model_psnr.mp4')
