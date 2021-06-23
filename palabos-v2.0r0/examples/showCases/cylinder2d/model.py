a56c0f96c4efe584efa058fd2dfad4652af2062b
David Alexander <david.alexander@etu.unige.ch>
2021-06-23 08:55:57 +0200

    bilinear

--- palabos-v2.0r0/examples/showCases/cylinder2d/model.py
+++ palabos-v2.0r0/examples/showCases/cylinder2d/model.py
@@ -51,7 +51,7 @@ x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 x = Conv2D(1, (3, 3), activation='relu', padding='same')(x)
 
 upsample = Model(input_img, x)
-upsample.compile(optimizer='adadelta', loss="PSNR", metrics=[PSNR])
+upsample.compile(optimizer='adadelta', loss=PSNR, metrics=[PSNR])
 
 # Save the model
 plot_model(upsample,show_shapes=True, to_file='model_large.png')
